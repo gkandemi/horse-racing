809b5d4217ff94870e74da75d8278a0d90e45dc5
gkandemi <gokhan@gkandemir.com>
2024-12-11 03:33:05 +0300

    Deployment bug fixed and removed old files.

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import {fileURLToPath, URL} from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/horse-racing/' : '/', // Sadece production'da base ayarlanır
   server: {
     port: 3000
   },
