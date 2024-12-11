440d35af137511731230b424ac7cbba7c03e3c1d
gkandemi <gokhan@gkandemir.com>
2024-12-11 03:10:06 +0300

    deployment added.

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import {fileURLToPath, URL} from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/horse-racing/',
   server: {
     port: 3000
   },
