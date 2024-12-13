627aac092db8e08fe0435ccf4b5ebb15911c0b8e
gkandemi <gokhan@gkandemir.com>
2024-12-13 12:06:35 +0300

    build

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  transform: {
-    "^.+\\.js$": "babel-jest", // Babel ile ES Modules desteği
-  },
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1", // Alias yapılandırması (Vue projelerinde yaygın)
-  },
-};
--- src/utils/registerPlugins.js
+++
@@ -1,6 +0,0 @@
-import vSmoothScroll from 'v-smooth-scroll';
-
-export default function (app){
-  app.use(vSmoothScroll);
-
-}
